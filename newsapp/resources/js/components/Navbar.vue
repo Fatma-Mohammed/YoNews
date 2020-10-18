735821a13a4e7e4493929ff1598aaf8a3f5e7fcc
Fatma Mohammed <gfatmamohammed@gmail.com>
2020-10-18 19:59:20 +0200

    Add some navbar choices and style

--- newsapp/resources/js/components/Navbar.vue
+++ newsapp/resources/js/components/Navbar.vue
@@ -1,8 +1,76 @@
 <template>
-    <nav class="navbar navbar-expand-sm navbar-dark bg-info mb-2">
-        <div class="container">
-            <a href="#" class="navbar-brand">YoNews</a>
-            <router-link to="/EG-sports">Egypt Sport</router-link>
-        </div>
-    </nav>
+<div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="navbar-bottom">
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="nav-item">
+                <a id="navbar-brand" href="#"><i class="fab fa-hacker-news"></i>oNews</a>
+                </div>
+                <div>
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div
+                    class="navbar-collapse justify-content-center collapse"
+                    id="navbarSupportedContent"
+                >
+                    <ul
+                    class="navbar-nav d-lg-flex justify-content-between align-items-center"
+                    >
+                    <li>
+                        <button class="navbar-close">
+                        <i class="mdi mdi-close"></i>
+                        </button>
+                    </li>
+                    <li class="nav-item active">
+                        <router-link to="/sports" class="nav-link">Sport</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/business" class="nav-link">Business</router-link>
+                    </li>
+                    </ul>
+                   
+                </div>
+                </div>
+                <ul class="social-media">
+                <li>
+                    <button @click="$emit('chCountry','eg')">
+                    <img src='/imgs/egypt-icon.png' />
+                    </button>
+                </li>
+                <li>
+                    <button @click="$emit('chCountry','ae')">
+                    <img src='/imgs/united-arab-emirates-icon.png' />
+
+                    </button>
+                </li>
+                <li>
+                    <a href="#">
+                    <i class="mdi mdi-twitter"></i>
+                    </a>
+                </li>
+                </ul>
+                 <ul class="navbar-top-right-menu">
+                    <li class="nav-item">
+                      <a href="#" class="nav-link"><i class="mdi mdi-magnify"></i></a>
+                    </li>
+                    <li class="nav-item">
+                      <a href="#" class="nav-link">Login</a>
+                    </li>
+                    <li class="nav-item">
+                      <a href="#" class="nav-link">Sign in</a>
+                    </li>
+                  </ul>
+            </div>
+            
+            </div>
+        </nav>
+    </div>
 </template>
--- newsapp/resources/views/welcome.blade.php
+++ newsapp/resources/views/welcome.blade.php
@@ -12,12 +12,12 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
+    <script src="https://kit.fontawesome.com/7760f1a75d.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/css/all.css">
 </head>
 
 <body>
     <div id="app">
-        <Navbar></Navbar>
         <mainapp></mainapp>
     </div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
