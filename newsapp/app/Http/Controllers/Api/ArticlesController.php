cad2470bf3e28ba8f27b294e070ae2e785206419
Fatma Mohammed <gfatmamohammed@gmail.com>
2020-10-18 20:05:41 +0200

    Finalize basic fetch method

--- newsapp/app/Http/Controllers/Api/ArticlesController.php
+++ newsapp/app/Http/Controllers/Api/ArticlesController.php
@@ -9,13 +9,27 @@ use Illuminate\Support\Facades\Http;
 class ArticlesController extends Controller
 {
     function list(){
-        $response = Http::get('https://newsapi.org/v2/top-headlines', [
-            'country' => 'us',
-            'apiKey' => '0dda770125cf43f78af1759297137d30',
-        ]);
-        if($response->ok()){
-            return $response->json()['articles']; 
+        //If no country provided then default country is egypt
+        $country = request('country') ? request('country') : 'eg';
+        $categories = ['sports','business'];
+        $articles = [];
+        //for each category in system fetch api data
+        foreach ($categories as $category) {
+            $response = Http::get('https://newsapi.org/v2/top-headlines', [
+            
+                'country' => $country,
+                'category' => $category,
+                'apiKey' => '0dda770125cf43f78af1759297137d30',
+    
+            ]);
+            if($response->ok()){
+                //insert request articles for each loop to our articles array
+                $articles = array_merge($articles, $response['articles']);
+                
+            }
         }
+        return $articles;
+
 
     }
 }
