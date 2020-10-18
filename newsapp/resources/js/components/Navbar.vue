fdde934243f10736e6f51cda6e265f39607bbd91
Fatma Mohammed <gfatmamohammed@gmail.com>
2020-10-18 03:06:28 +0200

    Add basic navbar element

---
+++ newsapp/resources/js/components/Navbar.vue
@@ -0,0 +1,8 @@
+<template>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-info mb-2">
+        <div class="container">
+            <a href="#" class="navbar-brand">YoNews</a>
+            <router-link to="/EG-sports">Egypt Sport</router-link>
+        </div>
+    </nav>
+</template>
