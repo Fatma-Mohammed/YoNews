7fd08114a9a608993dc7c80858a975f748358554
Fatma Mohammed <gfatmamohammed@gmail.com>
2020-10-19 06:38:57 +0200

    Fix sanctum configuration

--- newsapp/app/Providers/RouteServiceProvider.php
+++ newsapp/app/Providers/RouteServiceProvider.php
@@ -72,9 +72,10 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::prefix('api')
-            ->middleware('api')
-            ->namespace($this->namespace)
-            ->group(base_path('routes/api.php'));
+        // Route::prefix('api')
+        //     ->middleware('api')
+        //     ->namespace($this->namespace)
+        //     ->group(base_path('routes/api.php'));
+        Route::prefix('api')->middleware(['web'])->namespace($this->namespace)->group(base_path('routes/api.php'));
     }
 }
