dcf5eb3acd5dd2eba3f9df2c21fd76ab3a6d1b16
Fatma Mohammed <gfatmamohammed@gmail.com>
2020-10-19 06:41:33 +0200

    Create home page component

---
+++ newsapp/resources/js/Pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="content-wrapper">
+        <Hero></Hero>
+        <Articles :articles="this.articles"></Articles>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: ["articles"]
+};
+
+</script>
